420dceeade980ab748ae4b520131e26a7e48639a
waresujay4 <waresujay4@gmail.com>
2025-05-31 21:52:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
 #(©)Codexbotz
-import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1002074173405
-pyrogram.utils.MIN_CHANNEL_ID = -1002022425228
 from aiohttp import web
 from plugins import web_server
 
-import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
